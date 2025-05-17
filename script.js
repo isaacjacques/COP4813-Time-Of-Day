c50f6de38e3a646b5bc8d9f9483f166f4d6a5ea7
isaacjacques <jacques.development@outlook.com>
2025-05-17 13:09:40 -0400

    separate javascript file

---
+++ script.js
@@ -0,0 +1,22 @@
+const hour = new Date().getHours();
+let backgroundColor;
+
+const COLORS = {
+    morning: "#994d00",    
+    afternoon: "#007399", 
+    evening: "#260099",
+    night: "#730099"
+};
+
+
+if (hour >= 6 && hour < 12) {
+    backgroundColor = COLORS.morning;
+} else if (hour >= 12 && hour < 18) {
+    backgroundColor = COLORS.afternoon;
+} else if (hour >= 18 && hour < 21) {
+    backgroundColor = COLORS.evening;
+} else {
+    backgroundColor = COLORS.night;
+}
+
+document.body.style.backgroundColor = backgroundColor;
